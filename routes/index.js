4995e4f38ecdcc7c92102f5351efef0ae6ef551a
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-27 17:15:42 +0530

    User model and post model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/createUser", async function (req, res, next) {
+  let createdUser = await userModel.create({
+    username: "User1",
+    password: "user1@123",
+    posts: [],
+    email: "user1@gmail.com",
+    fullName: "User Roy",
+  });
+
+  res.send(createdUser);
+});
+
+router.get("/createPost", async function (req, res, next) {
+  let createdPost = await postModel.create({
+    postText: "Hello, I am User1",
+  });
+
+  res.send(createdPost);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect("mongodb://localhost:27017/Pinterest");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [],
+  dp: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+  },
 });
 
-module.exports = router;
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
