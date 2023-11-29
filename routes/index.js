fc1e1dd76d59810a809df358e9f162f44c102d8e
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-29 06:47:57 +0530

    Passport setup for profile register

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,25 @@
 var express = require("express");
 var router = express.Router();
 const userModel = require("./users");
-const postModel = require("./posts");
+const passport = require("passport");
+
+const localStrategy = require("passport-local");
+passport.authenticate(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
-router.get("/allUserPosts", async function (req, res, next) {
-  let user = await userModel
-    .findOne({ _id: "656487e04d9171478fb40c85" })
-    .populate("posts");
-  res.send(user);
-});
 
-router.get("/createUser", async function (req, res, next) {
-  let createdUser = await userModel.create({
-    username: "User1",
-    password: "user1@123",
-    posts: [],
-    email: "user1@gmail.com",
-    fullName: "User Roy",
+router.post("/register", function (req, res) {
+  const { username, email, fullName } = req.body;
+
+  const userData = new userModel({ username, email, fullName });
+
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
   });
-
-  res.send(createdUser);
 });
-
-router.get("/createPost", async function (req, res, next) {
-  let createdPost = await postModel.create({
-    postText: "Hello, how are you?",
-    user: "656487e04d9171478fb40c85",
-  });
-
-  let user = await userModel.findOne({ _id: "656487e04d9171478fb40c85" });
-  user.posts.push(createdPost._id);
-  await user.save();
-  res.send("Done!");
-});
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost:27017/Pinterest");
 
@@ -30,6 +31,8 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+userSchema.plugin(plm);
 
 const User = mongoose.model("User", userSchema);
 
