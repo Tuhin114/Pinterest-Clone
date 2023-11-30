0bce101c498161d64a43b6c64c7edb78023809ca
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-11-30 08:24:57 +0530

    Completed Login section

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,12 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get("/profile", isLoggedIn, function (req, res, next) {
+  res.send("profile");
+});
+
 router.post("/register", function (req, res) {
   const { username, email, fullName } = req.body;
-
   const userData = new userModel({ username, email, fullName });
 
   userModel.register(userData, req.body.password).then(function () {
@@ -22,4 +25,29 @@ router.post("/register", function (req, res) {
     });
   });
 });
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  }),
+  function (req, res) {}
+);
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
 module.exports = router;
